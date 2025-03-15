54a97af53d293556a541891aa0576d35e59ec46b
Michael Odusami <kyrmikee@gmail.com>
2025-03-15 12:17:48 -0400

    Time: 16 ms (25.00%) | Memory: 26.7 MB (9.76%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.swift
+++ 217-contains-duplicate/contains-duplicate.swift
@@ -1,6 +1,16 @@
 class Solution {
     func containsDuplicate(_ nums: [Int]) -> Bool {
         // if every item is unique, this means that we can utilize a set
-        return Set(nums).count != nums.count
+        // 1st solution: return Set(nums).count != nums.count
+
+        var map: [Int: Int] = [:]
+        for i in 0..<nums.count {
+            if let presentValue = map[nums[i]] {
+                return true
+            }
+            map[nums[i], default: 0] += 1
+        }
+        
+        return false
     }
 }
