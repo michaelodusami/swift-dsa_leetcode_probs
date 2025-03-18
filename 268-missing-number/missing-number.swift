58d11b2495a4c668467bdf3ab9ae490df2654140
Michael Odusami <kyrmikee@gmail.com>
2025-03-17 20:57:15 -0400

    Time: 0 ms (100.00%) | Memory: 20.4 MB (9.31%) - LeetSync

--- 268-missing-number/missing-number.swift
+++ 268-missing-number/missing-number.swift
@@ -1,15 +1,7 @@
 class Solution {
     func missingNumber(_ nums: [Int]) -> Int {
-        var n: Int = nums.count
-
-        // n is innclusive
-
-        for i in 0...n {
-            if nums.contains(i) == false {
-                return i
-            }
-        }
-
-        return -1
+        let expected: Int = (1...nums.count).reduce(0, +)
+        let actual: Int = nums.reduce(0, +)
+        return expected - actual
     }
 }
