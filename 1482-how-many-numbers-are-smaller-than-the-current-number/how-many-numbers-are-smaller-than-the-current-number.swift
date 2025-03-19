4ba4aca31fe3d0f16512e61a1d81cd1d4d49158c
Michael Odusami <kyrmikee@gmail.com>
2025-03-19 12:43:15 -0400

    Time: 1 ms (51.52%) | Memory: 20.1 MB (21.21%) - LeetSync

--- 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.swift
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/how-many-numbers-are-smaller-than-the-current-number.swift
@@ -1,18 +1,7 @@
 class Solution {
     func smallerNumbersThanCurrent(_ nums: [Int]) -> [Int] {
-        // READ Q2
-        // ExAMPLES AND NOTES
-        // REVERSE ENGINEER
-        var s: [Int] = []
-        for i in 0..<nums.count {
-            var count: Int = 0
-            for j in 0..<nums.count {
-                if i != j && nums[j] < nums[i] {
-                    count += 1
-                }
-            }
-            s.append(count)
+        return nums.map {
+            [sorted = nums.sorted()] num in sorted.firstIndex(of: num)!
         }
-        return s
     }
 }
