ca8b9ee407b44feeba813342a35a3c6bd41832f8
Michael Odusami <kyrmikee@gmail.com>
2025-03-18 22:01:20 -0400

    Time: 0 ms (100.00%) | Memory: 20 MB (19.97%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.swift
+++ 88-merge-sorted-array/merge-sorted-array.swift
@@ -1,40 +1,35 @@
 class Solution {
     func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-        // two pointers
-        // array is already sorted so we can merge from the back
-        // need two pointers at both arrays at the end
-        // one pointer for positioning
-        // we check which element in the array is greater, the greater element gets in the position with it's designated array pointer being decrease
-        // when do we stop?: when ptr is at m
-
-        var ptrOne: Int = m - 1
-        var ptrTwo: Int = n - 1
+        
         var ptr: Int = m + n - 1
+        var i: Int = m - 1
+        var j: Int = n - 1
 
-        // Merge from the end of both arrays
-        while ptrTwo >= 0 {
-           
-            if ptrOne >= 0 && nums1[ptrOne] > nums2[ptrTwo] {
-                nums1[ptr] = nums1[ptrOne]
-                ptrOne -= 1
-            } else {
-                nums1[ptr] = nums2[ptrTwo]
-                ptrTwo -= 1
+        while j >= 0 {
+
+            if i >= 0 && nums1[i] >= nums2[j] {
+                nums1[ptr] = nums1[i]
+                i -= 1
+            }
+            else {
+                nums1[ptr] = nums2[j]
+                j -= 1
             }
             ptr -= 1
-          
         }
 
-        print(nums1)
+        // go until nums1 or nums2 is at 0 (AND)
 
-        // If there are remaining elements in nums2, copy them over
-        while ptrTwo >= 0 {
-            nums1[ptr] = nums2[ptrTwo]
-            ptrTwo -= 1
-            ptr -= 1
-        }
-  
+        // we have to compare two numbers in an array together
+        // compare the last valid element in both arrays
+        // --- two pointer approach:
+        // 1. get the last two numbers, compare which is greater and slot the greater element into the last pos in the nums1 arr 
 
+        // a merged nums1 where there are no more elements to merge from nums2 in sorted order
 
     }
 }
+
+// READ Q Twice
+// READ Examples And Make Notes
+// Reverse Engineer
